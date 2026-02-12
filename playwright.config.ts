cb69a57eef4b75594de14ee6d3ec41cd6e8e4005
emarkula24 <emarkula24@student.oulu.fi>
2026-02-12 19:01:47 +0200

    Add the rest of the tests specified by assignment 5 instructions

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -36,7 +36,11 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+        launchOptions: {
+          executablePath: "/usr/bin/chromium",
+        }
+       },
     },
 
     {
@@ -73,7 +77,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:5000',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
+  
 });
--- tests/dog.spec.ts
+++ tests/dog.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect } from '@playwright/test'
 
-test.describe("Dog API frontend", () =>{
+test.describe("Random Dog", () =>{
     test("Dog image retrieved on page load", async ({page}) => {
 
         await page.goto('/')
@@ -16,10 +16,9 @@ test.describe("Dog API frontend", () =>{
     test("Dog image is retrieved on button press", async ({page}) => {
 
         await page.goto('/')
-        const responsePromise = page.waitForResponse('**/api/dogs/random')
-        await responsePromise
 
         await page.getByText("Get Another Dog").click()
+        const responsePromise = page.waitForResponse('**/api/dogs/random')
         await responsePromise
 
         const img = page.getByAltText("Random dog")
@@ -27,5 +26,21 @@ test.describe("Dog API frontend", () =>{
         const imgSrc = await img.getAttribute("src")
         expect(imgSrc).toMatch(/^https:\/\//)
 
+    })
+    test("Error message is shown when API call fails", async ({page}) => {
+
+        
+        await page.route('**/api/dogs/random', async (route) => {
+            await route.abort()
+        })
+
+        await page.goto('/')
+        const errorElement = page.getByText(/error/i)
+        await expect(errorElement).toContainText(/error/i)
+        await expect(errorElement).toBeVisible()
+        
+
+        
+        
     })
 })
